41b9a454c37de5a225f0986c8279bc18df0169f5
Ariel Nunes <arielsn@outlook.com>
2017-05-24 09:33:16 -0300

    Continuação no desenvolvimento do projeto do curso
    Seção 3 - Aula 21 - Adding Create User Logic concluida

--- src/main/java/com/bookstore/controller/HomeController.java
+++ src/main/java/com/bookstore/controller/HomeController.java
@@ -1,24 +1,42 @@
 package com.bookstore.controller;
 
+import java.util.HashSet;
 import java.util.Locale;
+import java.util.Set;
+import java.util.UUID;
+
+import javax.servlet.http.HttpServletRequest;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import com.bookstore.domain.User;
 import com.bookstore.domain.security.PasswordResetToken;
+import com.bookstore.domain.security.Role;
+import com.bookstore.domain.security.UserRole;
 import com.bookstore.service.UserService;
 import com.bookstore.service.Impl.UserSecurityService;
+import com.bookstore.utility.MailConstructor;
+import com.bookstore.utility.SecurityUtility;
 
 @Controller
 public class HomeController {
+	@Autowired
+	private JavaMailSender mailSender;
+
+	@Autowired
+	private MailConstructor mailConstructor;
 
 	@Autowired
 	private UserService userService;
@@ -40,6 +58,55 @@ public class HomeController {
 	@RequestMapping("/forgetPassword")
 	public String forgetPassword(Model model) {
 		model.addAttribute("classActiveForgetPassword", true);
+		return "myAccount";
+	}
+
+	@RequestMapping(value = "/newUser", method = RequestMethod.POST)
+	private String newUserPost(HttpServletRequest request, @ModelAttribute("email") String userEmail,
+			@ModelAttribute("username") String username, Model model) throws Exception {
+
+		model.addAttribute("classActiveNewAccout", true);
+		model.addAttribute("email", userEmail);
+		model.addAttribute("username", username);
+
+		if (userService.findByUsername(username) != null) {
+			model.addAttribute("usernameExists", true);
+
+			return "myAccount";
+		}
+
+		if (userService.findByEmail(userEmail) != null) {
+			model.addAttribute("email", true);
+
+			return "myAccount";
+		}
+
+		User user = new User();
+		user.setUsername(username);
+		user.setEmail(userEmail);
+
+		String password = SecurityUtility.randomPasswordGenerator();
+		String encryptedPassword = SecurityUtility.passwordEncoder().encode(password);
+		user.setPassword(encryptedPassword);
+
+		Role role = new Role();
+		role.setRoleId(1);
+		role.setName("ROLE_USER");
+		Set<UserRole> userRoles = new HashSet<>();
+		userRoles.add(new UserRole(user, role));
+
+		userService.createUser(user, userRoles);
+
+		String token = UUID.randomUUID().toString();
+
+		userService.createPasswordResetTokenForUser(user, token);
+		String appUrl = "http://" + request.getServerName() + ":" + request.getServerPort() + request.getContextPath();
+
+		SimpleMailMessage email = mailConstructor.constructResetTokenEmail(appUrl, request.getLocale(), token, user,
+				password);
+
+		mailSender.send(email);
+		model.addAttribute("emailSent", true);
 		return "myAccount";
 	}
 
--- src/main/java/com/bookstore/service/Impl/UserServiceImpl.java
+++ src/main/java/com/bookstore/service/Impl/UserServiceImpl.java
@@ -1,11 +1,16 @@
 package com.bookstore.service.Impl;
 
+import java.util.Set;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.bookstore.domain.User;
 import com.bookstore.domain.security.PasswordResetToken;
+import com.bookstore.domain.security.UserRole;
 import com.bookstore.repository.PasswordResetTokenRepository;
+import com.bookstore.repository.RoleRepository;
+import com.bookstore.repository.UserRepository;
 import com.bookstore.service.UserService;
 
 @Service
@@ -13,6 +18,12 @@ public class UserServiceImpl implements UserService {
 
 	@Autowired
 	private PasswordResetTokenRepository passwordResetTokenRepository;
+
+	@Autowired
+	private UserRepository userRepository;
+
+	@Autowired
+	private RoleRepository roleRepository;
 
 	@Override
 	public PasswordResetToken getPasswordResetToken(final String token) {
@@ -23,6 +34,33 @@ public class UserServiceImpl implements UserService {
 	public void createPasswordResetTokenForUser(final User user, final String token) {
 		final PasswordResetToken myToken = new PasswordResetToken(token, user);
 		passwordResetTokenRepository.save(myToken);
+	}
+
+	@Override
+	public User findByUsername(String username) {
+		return userRepository.findByUsername(username);
+	}
+
+	@Override
+	public User findByEmail(String email) {
+		return userRepository.findByEmail(email);
+	}
+
+	@Override
+	public User createUser(User user, Set<UserRole> userRoles) throws Exception {
+		User localUser = userRepository.findByUsername(user.getUsername());
+
+		if (localUser != null) {
+			throw new Exception("User already exists. Nothing will be done.");
+		} else {
+			for (UserRole ur : userRoles) {
+				roleRepository.save(ur.getRole());
+			}
+
+			user.getUserRoles().addAll(userRoles);
+			localUser = userRepository.save(user);
+		}
+		return localUser;
 	}
 
 }
