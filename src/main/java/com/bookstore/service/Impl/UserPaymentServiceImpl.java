bdbd5037d88eab131fceb6793f87f941007c002a
Ariel Nunes <arielsn@outlook.com>
2017-06-19 08:55:03 -0300

    Continuação no desenvolvimento do projeto
    Seção 6 - Aula 47 - Bookstore: Add Update CreditCard Info concluída

---
+++ src/main/java/com/bookstore/service/Impl/UserPaymentServiceImpl.java
@@ -0,0 +1,21 @@
+package com.bookstore.service.Impl;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.bookstore.domain.UserPayment;
+import com.bookstore.repository.UserPaymentRepository;
+import com.bookstore.service.UserPaymentService;
+
+@Service
+public class UserPaymentServiceImpl implements UserPaymentService {
+
+	@Autowired
+	private UserPaymentRepository userPaymentRepository;
+
+	@Override
+	public UserPayment findById(Long id) {
+		return userPaymentRepository.findOne(id);
+	}
+
+}
